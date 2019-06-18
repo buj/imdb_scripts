1e8c9d849eec1bd5c119129178c7fe32516b9557
Truc Lam Bui <bujtuclam@gmail.com>
2019-06-18 14:05:01 +0200

    fix (?) torch util, comments to pretrain_lm.py

--- torch_util.py
+++ torch_util.py
@@ -2,7 +2,7 @@ import torch
 
 
 def make_cuda(tensor):
-  """Turn the tensor into cuda if possible."""
-  if torch.cuda.current_device() != -1:
-    return tensor.cuda()
-  return tensor
+    """Turn the tensor into cuda if possible."""
+    if torch.cuda.is_available():
+        return tensor.cuda()
+    return tensor
